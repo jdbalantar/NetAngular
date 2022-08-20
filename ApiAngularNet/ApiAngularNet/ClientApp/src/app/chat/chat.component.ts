ed5d991b2321d96daf060d34467b8bb2b8e74919
jdbalantar <jdbalantar@gmail.com>
2022-08-20 11:22:12 -0500

    Adding files to repo

---
+++ ApiAngularNet/ApiAngularNet/ClientApp/src/app/chat/chat.component.ts
@@ -0,0 +1,9 @@
+﻿import { Component } from '@angular/core';
+
+@Component({
+    selector: 'chat-app',
+    templateUrl: './chat.component.html'
+})
+export class ChatComponent {
+    public lstMessages: string[] = ["Hola mundo", "David xd", "pica papas"];
+}
